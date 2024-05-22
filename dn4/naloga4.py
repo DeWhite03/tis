e17de76f8166fb72060697c3c8a77e183acdd02b
DeWhite03 <denis.belic26@gmail.com>
2024-05-22 12:53:22 +0200

    commit

--- dn4/naloga4.py
+++ dn4/naloga4.py
@@ -1,4 +1,8 @@
 import numpy as np
+
+def DCT(x: int, N: int, n: int) -> float:
+    return 2 * np.sum([x*np.cos((np.pi/N)*(k+.5)*n) for k in range(N)])
+
 def naloga4(slika: np.array, velikostOkna: int) -> float:
     """
     Poenostavi sliko
@@ -15,6 +19,22 @@ def naloga4(slika: np.array, velikostOkna: int) -> float:
     PSNR : float
          Peak Signal to Noise Ratio
     """
+    print(slika)
+    P = np.zeros((len(slika), len(slika[0])), dtype=float)
+    print(P)
 
+    for ri, row in enumerate(slika):
+        for ei, element in enumerate(row):
+            P[ri][ei] = DCT(ei, velikostOkna, ei)
+    
+    print(P)
+    Pt = P.transpose()
+    print(Pt)
+    P2 = np.zeros((len(slika), len(slika[0])), dtype=float)
+    for ri, row in enumerate(Pt):
+        for ei, element in enumerate(row):
+            P2[ri][ei] = DCT(ei, velikostOkna, ei)
+
+    print(P2)
     PSNR = float('nan')
     return PSNR
