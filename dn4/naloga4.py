55f0704ea3a44fd9fabafdfdc0c912ed3a90d893
DeWhite03 <denis.belic26@gmail.com>
2024-05-22 12:00:02 +0200

    commit

---
+++ dn4/naloga4.py
@@ -0,0 +1,20 @@
+import numpy as np
+def naloga4(slika: np.array, velikostOkna: int) -> float:
+    """
+    Poenostavi sliko
+
+    Parameters
+    ----------
+    slika : numpy array
+        vhodna slika 
+    velikostOkna : int
+        velikost okna za DCT
+    
+    Returns
+    -------
+    PSNR : float
+         Peak Signal to Noise Ratio
+    """
+
+    PSNR = float('nan')
+    return PSNR
